0316236b82e2b216e54c8a33b202cdf2fd1242e2
Kargalex <kargiotisalexandros@gmail.com>
2025-04-01 18:32:17 +0300

    Correctly removed old assembly code

--- hash_interm.s
+++ hash_interm.s
@@ -2,38 +2,6 @@
 
 .global hash_interm
 
-/*
-	hash_interm:
-// r0 = num
-// r1 = &result
-	PUSH {r4-r6,lr}				// Save to stack r4-r6 and return address
-	MOV r4,#0					// r4 = sum = 0
-
-	LOOP:
-	CMP r0,#0		
-	BLE EXIT					// if num<=0, stop looping
-
-	MOV r6,#10
-	UDIV r2,r0,r6				// r2 = r0/10
-	MUL r3,r2,r6				// r3 = r2*10, this way r3=num-(num%10)
-	SUB r5,r0,r3				// r5 = num%10
-	ADD r4,r4,r5				// sum = sum + num%10
-	UDIV r0,r0,r6				// num = num / 10
-	B LOOP
-
-
-
-	EXIT:
-	MOV r6,#7
-	UDIV r5,r4,r6				//(int)r5=sum/7
-	MUL r5,r5,r6				// r5=r5*7, this way r5=sum-(sum%7)	
-	SUB r4,r4,r5				// r4 = sum%7
-
-	STRB r4,[r1]				// *result = sum
-	POP {r4-r6,pc}
-.end
-*/
-
 	hash_interm:
 // r0 = num
 // r1 = &result
